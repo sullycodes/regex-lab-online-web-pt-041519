ddf1ff82cbcc377cc685fc45c0bab86f9f63d24c
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-19 15:36:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[AaEeIiOoUu]/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un[a-z]+/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*[\.,:;]$/)
+    return true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.scan(/\b^([0-9]{10})$\b/)
+    true 
+  else 
+    false 
+  end
 end
